f57129c90ea09e122b9144fbe15588b5e822a20c
Talia Pichardo <taliapichardo@gmail.com>
2024-01-20 17:05:01 -0500

    Create participant table in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,17 @@ CREATE TABLE raffle (
     date_created TIMESTAMP NOT NULL,
     date_raffled TIMESTAMP
 );
+
+DROP TABLE IF EXISTS participant;
+
+CREATE TABLE participant (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL, 
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone_number TEXT,
+    is_winner BOOLEAN DEFAULT false,
+    lost BOOLEAN DEFAULT false,
+    raffle_id INT NOT NULL,
+    FOREIGN KEY (raffle_id) REFERENCES raffle (id)
+);
