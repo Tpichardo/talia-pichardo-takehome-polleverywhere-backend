d7e3a78f2d875a2388578b0db6701f164fc16805
Talia Pichardo <taliapichardo@gmail.com>
2024-01-20 22:18:50 -0500

    Edit seed data for raffle table

--- db/seed.sql
+++ db/seed.sql
@@ -3,6 +3,6 @@
 INSERT INTO 
     raffle (raffle_name, secret_token, date_created)
 VALUES
-    ('Talia', 'abc123', '2024-01-20 12:00:00'),
-    ('Edison', 'xyz456', '2024-01-21 14:30:00'),
-    ('Clara', '123abc', '2024-01-22 10:45:00');
+    ('Talia', 'abc123', '2024-01-18 12:00:00'),
+    ('Edison', 'xyz456', '2024-01-19 14:30:00'),
+    ('Clara', '123abc', '2024-01-20 10:45:00');
