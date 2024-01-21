aedce03285aa54c598a7d4e65a0ae0d274ef316d
Talia Pichardo <taliapichardo@gmail.com>
2024-01-20 19:22:20 -0500

    Create seed.sql and add data to seed raffle table

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c raffles;
+
+INSERT INTO 
+    raffle (raffle_name, secret_token, date_created)
+VALUES
+    ('Talia', 'abc123', '2024-01-20 12:00:00'),
+    ('Edison', 'xyz456', '2024-01-21 14:30:00'),
+    ('Clara', '123abc', '2024-01-22 10:45:00');
