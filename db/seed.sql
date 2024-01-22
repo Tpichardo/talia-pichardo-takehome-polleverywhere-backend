e0ef05e52ccf599118ff60b592896871b8ff50f1
Talia Pichardo <taliapichardo@gmail.com>
2024-01-22 09:43:20 -0500

    Add pgcrypto extension to be able to safely store passwords in db

--- db/seed.sql
+++ db/seed.sql
@@ -3,6 +3,6 @@
 INSERT INTO 
     raffle (raffle_name, secret_token, date_created)
 VALUES
-    ('Talia', 'abc123', '2024-01-18 12:00:00'),
-    ('Edison', 'xyz456', '2024-01-19 14:30:00'),
-    ('Clara', '123abc', '2024-01-20 10:45:00');
+    ('Talia', crypt('password', gen_salt('bf')), '2024-01-18 12:00:00'),
+    ('Edison', crypt('password', gen_salt('bf')), '2024-01-19 14:30:00'),
+    ('Clara', crypt('password', gen_salt('bf')), '2024-01-20 10:45:00');
