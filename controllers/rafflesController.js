c25ae5c0a6d8e52f0bc60e97e9a24958fa672d67
Talia Pichardo <taliapichardo@gmail.com>
2024-01-22 11:39:56 -0500

    Add get raffle by id route

--- controllers/rafflesController.js
+++ controllers/rafflesController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllRaffles } = require("../queries/raffles");
+const { getAllRaffles, getRaffleById } = require("../queries/raffles");
 
 const raffles = express.Router();
 
@@ -7,6 +7,16 @@ raffles.get("/", async (request, response) => {
 	try {
 		const allRaffles = await getAllRaffles();
 		response.status(200).json(allRaffles);
+	} catch (error) {
+		return error;
+	}
+});
+
+raffles.get("/:id", async (request, response) => {
+	const { id } = request.params;
+	try {
+		const raffle = await getRaffleById(id);
+		response.status(200).json(raffle);
 	} catch (error) {
 		return error;
 	}
